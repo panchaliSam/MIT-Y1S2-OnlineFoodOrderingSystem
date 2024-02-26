44875d9e3cdf65b275e865b63408fafcb04add3e
chathumiamarasinghe <chathumiamarasinghe@gmail.com>
2024-02-26 09:32:34 +0530

    Vehicle class is modified and the db connection added

--- foodOrderingSystem/src/main/java/com/bs/model/Vehicle.java
+++ foodOrderingSystem/src/main/java/com/bs/model/Vehicle.java
@@ -1,13 +1,90 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package main.java.com.bs.model;
 
 /**
  *
- * @author User
+ * @author Group4 - Chathumi
  */
 public class Vehicle {
+     //Attributes of Vehicle class
+    private int vehicleId;
+    private String type;
+    private String registrationNumber;
+    private int model;
+    private boolean isActive;
+
     
+    // Default constructor
+    public Vehicle() {
+    }
+
+    // parameteric constructor
+    public Vehicle(int vehicleId,String type, String registrationNumber,String email,int model,boolean isActive) {
+        
+        this.vehicleId = vehicleId;
+        this.type = type;
+        this.registrationNumber = registrationNumber;
+        this.model = model;
+        this.isActive = isActive;
+        
+    }
+
+
+    // Getter and Setter for vehicleId
+    public int getvehicleId() {
+        return vehicleId;
+    }
+
+    public void setvehicleId(int vehicleId) {
+        this.vehicleId = vehicleId;
+    }
+
+    // Getter and Setter for type
+    public String gettype() {
+        return type;
+    }
+
+    public void settype(String type) {
+        this.type = type;
+    }
+
+    // Getter and Setter for regnumber
+    public String getregistrationNumber() {
+        return registrationNumber;
+    }
+    public void setregistrationNumber(String registrationNumber) {
+        this.registrationNumber = registrationNumber;
+    }
+    
+     public void getmodel(int model) {
+        this.model = model;
+    }
+    public void setmodel(int model) {
+        this.model = model;
+    }
+
+    // Getter and Setter for isActive
+    public boolean getIsActive() {
+        return isActive;
+    }
+
+    public void setIsActive(boolean isActive) {
+        this.isActive = isActive;
+    }
+    
+    
+    @Override
+    public String toString() {
+        return "vehicletest{" +
+            "vehicleId=" + vehicleId +
+            ", type='" + type + '\'' +
+            ", registrationNumber='" + registrationNumber + '\'' +
+            ", model='" + model + '\'' +
+            ", isActive=" + isActive +
+            
+            '}';
+    }
+     
 }
+
+
