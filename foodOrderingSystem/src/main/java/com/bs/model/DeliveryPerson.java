3ffb5bbc5ed16f93653a11cd5a9c2a67c2f53582
NisalaDineth <nisaladineth2001@gmial.com>
2024-02-27 18:59:33 +0530

    DP model class

--- foodOrderingSystem/src/main/java/com/bs/model/DeliveryPerson.java
+++ foodOrderingSystem/src/main/java/com/bs/model/DeliveryPerson.java
@@ -1,13 +1,63 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package main.java.com.bs.model;
 
 /**
  *
- * @author User
+ * @author Group4 - Nisala
  */
-public class DeliveryPerson {
+
+public class DeliveryPerson extends Person {
     
+    private int DeliveryPersonId;
+    private boolean isActive;
+    
+
+
+    //Default Constructor
+    public DeliveryPerson() {
+
+    }
+
+    //Parametric Constructor
+    public DeliveryPerson(int DeliveryPersonId, String firstName, String lastName, String email, String telNo, boolean isActive){
+        super(firstName, lastName, email, telNo);
+        this.DeliveryPersonId = DeliveryPersonId;
+        this.isActive = isActive;
+    
+    }
+
+    // Getter and Setter for DeliveryPersonID
+    public int getDeliveryPersonId() {
+        return DeliveryPersonId;
+    }
+
+    public void setDeliveryPersonId(int DeliveryPersonId) {
+        this.DeliveryPersonId = DeliveryPersonId;
+    }
+    
+    // Getter and Setter for isActive
+    public boolean getisActive() {
+        return isActive;
+    }
+    
+    public void setisActive(boolean isActive) {
+        this.isActive = isActive;
+    }
+    
+    @Override
+    public String toString() {
+        return "DeliveryPerson{" +
+                "DeliveryPersonId=" + DeliveryPersonId +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", telNo='" + telNo + '\'' +
+                ", isActive=" + isActive +
+                
+                '}';
+    }
+    
+    @Override
+    public void myTask() {
+        System.out.println("Delivery Person can select a vehicle and deliver the order.");
+    }
 }
