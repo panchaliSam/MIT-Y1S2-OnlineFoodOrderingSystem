14e1fec482701377bb7fd031b47b0c4f5556f581
panchaliSam <panchalisam20030117@gmail.com>
2024-02-27 19:32:39 +0530

    changes

--- foodOrderingSystem/src/main/java/com/bs/model/DeliveryPerson.java
+++ foodOrderingSystem/src/main/java/com/bs/model/DeliveryPerson.java
@@ -8,6 +8,58 @@ package main.java.com.bs.model;
  *
  * @author User
  */
-public class DeliveryPerson {
+public class DeliveryPerson extends Person{
+            private int DeliveryPersonId;
+    private boolean isActive;
     
+
+
+    //Default Constructor
+    public DeliveryPerson () {
+
+    }
+
+    //Parametric Constructor
+    public DeliveryPerson (int DeliveryPersonId, String firstName, String lastName, String email, String telNo, boolean isActive){
+        super(firstName, lastName, email, telNo);
+        this.DeliveryPersonId = DeliveryPersonId;
+        this.isActive = isActive;
+    
+    }
+
+    // Getter and Setter for DeliveryPersonID
+    public int getDeliveryPersonId() {
+        return DeliveryPersonId;
+    }
+
+    public void setDeliveryPersonId(int DeliveryPersonId) {
+        this.DeliveryPersonId = DeliveryPersonId;
+    }
+    
+    // Getter and Setter for isActive
+    public boolean getisActive() {
+        return isActive;
+    }
+    
+    public void setisActive(boolean isActive) {
+        this.isActive = isActive;
+    }
+    
+    @Override
+    public String toString() {
+        return "DeliveryPerson(" +
+                "DeliveryPersonId=" + DeliveryPersonId +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", telNo='" + telNo + '\'' +
+                ", isActive=" + isActive +
+                
+                '}';
+    }
+    
+    @Override
+    public void myTask() {
+        System.out.println("Delivery Person can select a vehicle and deliver the order.");
+    }
 }
